2d33789062cfdd13f3463d66686dc31f1bb939e6
juanhposso <juan.hernandez0793@gmail.com>
2021-08-17 16:23:05 -0500

    proyecto terminado usando async/await

--- src/JS/main.js
+++ src/JS/main.js
@@ -2,19 +2,40 @@ let arrayNumbers = []; // Agregamos los numeros sin repetir
 
 // * Realiza las peticiones a la api pasa como parametro el array
 async function nombres(number) {
-	number.forEach(async (idCharacter) => {
-		const URL = `https://rickandmortyapi.com/api/character/${idCharacter}`;
+	/* const URL = `https://rickandmortyapi.com/api/character/${number}`;
+	const info = await fetch(URL);
+	const res = await info.json();
 
+	const imgSrc = res.image;
+	createHTML(imgSrc); */
+
+	number.forEach(async (idCharacter) => {
+		//console.log(idCharacter);
+		const URL = `https://rickandmortyapi.com/api/character/${idCharacter}`;
+		console.log(URL);
 		const info = await fetch(URL);
 		const res = await info.json();
-		//console.log(res.image);
-		const imgSrc = res.image;
-		createHTML(imgSrc);
+
+		const {
+			image,
+			name,
+			origin: { url: urlLocation },
+		} = res;
+
+		if (urlLocation === '') {
+			createHTML(image, name);
+		} else {
+			const urlDimension = await fetch(urlLocation);
+			const dimensionResponse = await urlDimension.json();
+			console.log(dimensionResponse.dimension);
+
+			createHTML(image, name, dimensionResponse.dimension);
+		}
 	});
 }
 
 // * renderizar el HTML
-function createHTML(imgLink) {
+function createHTML(imgLink, characterName, dime = 'unknown') {
 	const sectionElement = document.querySelector('.carousel__container');
 	const divElement = document.createElement('div');
 	divElement.classList.add(
@@ -31,8 +52,16 @@ function createHTML(imgLink) {
 	);
 	divElement.innerHTML = `
 	<img src="${imgLink}" alt="imagen jeje" class="object-cover w-full h-full">
+	<div class="infoImg bg-black absolute -bottom-0 w-full text-white pl-3 pb-1 opacity-0">
+		<span class="text-xl font-semibold">Nombre: </span> <p class="mb-3">${characterName}</p>
+		<span class="text-xl font-semibold"> Dimension: </span> <p>${dime}</p></div>
 	`;
 
+	// * Div sobre info del personaje
+	/* 	const imgAboutInfo = document.createElement('div');
+	imgAboutInfo.classList.add('bg-gray-400', 'w-full', 'h-28', 'z-30');
+
+	divElement.appendChild(imgAboutInfo); */
 	sectionElement.appendChild(divElement);
 }
 
